8490811633b777c17f7a7fa001e6f8053732e019
KOleksandr <kereya_ua@ukr.net>
2014-10-21 16:14:49 +0300

    Update MySqlDAOFactory.java

--- src/main/java/status/db/MySqlDAOFactory.java
+++ src/main/java/status/db/MySqlDAOFactory.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package status.db;
 
 
@@ -25,13 +19,6 @@ public class MySqlDAOFactory implements DAOFactory {
     private String url = "jdbc:mysql://localhost:3306/server";//URL адреса 
     private String driver = "com.mysql.jdbc.Driver";//Ім'я драйвера 
     
-    
-    
-    /**
-     *
-     * @return
-     * @throws SQLException
-     */
     @Override
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
